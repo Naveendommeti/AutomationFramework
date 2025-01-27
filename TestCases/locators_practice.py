327ea1a89c35ee8edd69cb3406c3abd03161900a
Naveendommeti <naveendommety99@gmail.com>
2025-01-27 22:39:45 +0530

    commit

---
+++ TestCases/locators_practice.py
@@ -0,0 +1,19 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Option 1: Using driver.maximize_window()
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.orangehrm.com/")
+# driver.find_element(By.XPATH,"//input[@name='EmailHomePage']").send_keys("naveen")
+# driver.find_element(By.XPATH,"//input[@name='action_request']").click()
+# time.sleep(5)
+# driver.find_element(By.XPATH,"//button[@type='submit']").click()
+button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
+driver.execute_script("arguments[0].scrollIntoView(true);", button)
+time.sleep(5)
+# driver.quit()
